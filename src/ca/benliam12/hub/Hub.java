2622890c45784fc47271799ed681794c723e4820
BuildTools <unconfigured@null.spigotmc.org>
2018-07-22 18:11:04 -0400

    First Commit

---
+++ src/ca/benliam12/hub/Hub.java
@@ -0,0 +1,22 @@
+package ca.benliam12.hub;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class Hub extends JavaPlugin
+{
+
+    public static Logger log = Logger.getLogger("Minecraft");
+
+    public void onEnable()
+    {
+
+    }
+
+    public void onDisable()
+    {
+
+    }
+
+}
